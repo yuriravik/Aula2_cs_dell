cdaa0f1c60be38d458ddda8ac923bfcfc5a49c5b
ravik <ravik@LAPTOP-LCJ2GCBE>
2021-07-17 20:13:13 -0300

    Adicionar arquivos de projeto.

---
+++ Aula3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Aula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+        public class Cliente
+        {
+            public String cpf;
+        }
+        public class Pedido
+        {
+            public String dataPedido;
+            public String codigoPedido;
+            public Cliente cliente;
+            public ItemPedido item;
+
+            
+        }
+        public class ItemPedido
+        {
+            public int codigoItem;
+            public String descricaoItem;
+            public float valorItem;
+        }
+    }
+}
+
